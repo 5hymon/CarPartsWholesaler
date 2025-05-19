a244fa033e7bf8bd338c028b7e7b197681e80fd9
5hymon <245843@edu.p.lodz.pl>
2025-05-19 15:37:41 +0200

    App config

---
+++ src/main/java/frontend/src/app/parts-list/parts-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parts-list',
+  imports: [],
+  templateUrl: './parts-list.component.html',
+  styleUrl: './parts-list.component.css'
+})
+export class PartsListComponent {
+
+}
---
+++ src/test/java/com/wholesaler/backend/CarPartsWholesalerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wholesaler.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarPartsWholesalerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
