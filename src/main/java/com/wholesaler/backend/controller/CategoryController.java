331049950492d3f0ea32e2961c2d9c54969b1d04
kamilkazmierczak <245839@edu.p.lodz.pl>
2025-05-21 19:01:14 +0200

    Add a Category Controller

---
+++ src/main/java/com/wholesaler/backend/controller/CategoryController.java
@@ -0,0 +1,68 @@
+package com.wholesaler.backend.controller;
+
+import com.wholesaler.backend.model.Category;
+import com.wholesaler.backend.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryController {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    // GET /categories/all - get all categories
+    @GetMapping("/all")
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    // GET /categories/{categoryId} - get category by ID
+    @GetMapping("{categoryId}")
+    public ResponseEntity<Category> getCategory(@PathVariable("categoryId") Integer categoryId) {
+        Optional<Category> category = categoryRepository.findById(categoryId);
+        if (category.isPresent()) {
+            return new ResponseEntity<>(category.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    // POST - add new category
+    @PostMapping
+    public Category addCategory(@RequestBody Category category) {
+        return categoryRepository.save(category);
+    }
+
+    // PUT - update category by ID
+    @PutMapping("/{categoryId}")
+    public ResponseEntity<Category> updateCategory(@PathVariable("categoryId") Integer categoryId, @RequestBody Category updatedCategory) {
+        Optional<Category> optionalCategory = categoryRepository.findById(categoryId);
+        if (optionalCategory.isPresent()) {
+            Category category = optionalCategory.get();
+            category.setCategoryName(updatedCategory.getCategoryName());
+            category.setCategoryDescription(updatedCategory.getCategoryDescription());
+            return ResponseEntity.ok(categoryRepository.save(category));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    // DELETE - delete category by ID
+    @DeleteMapping("/{categoryId}")
+    public ResponseEntity<Void> deleteCategory(@PathVariable("categoryId") Integer categoryId) {
+        Optional<Category> category = categoryRepository.findById(categoryId);
+        if (category.isPresent()) {
+            categoryRepository.deleteById(categoryId);
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
